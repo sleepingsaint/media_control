a305b7bd8e7058b99d0f86d2fce1f7b5c6aba5cc
santosh <suryasantosh14523@gmail.com>
2025-05-03 16:10:25 +0530

    Updated version tag

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="media_control",
-    version="0.0.1",
+    version="0.0.2",
     description="Application to access media controls on your device",
     author="sleepingsaint",
     url="https://github.com/sleepingsaint/media_control",
